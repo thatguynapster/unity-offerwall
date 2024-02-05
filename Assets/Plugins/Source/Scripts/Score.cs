6eca1ffabdf4406a81a14c25f100d1661e231dca
thatGuyNapster <andrewosei94@gmail.com>
2024-02-05 13:42:56 +0000

    updated UI texts

---
+++ Assets/Plugins/Source/Scripts/Score.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GameObject scoreCounter;
+    public int score = 125;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
